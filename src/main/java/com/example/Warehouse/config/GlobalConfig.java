115c01ef345ee459b616af2a32e6a86d8b16bbe9
PC271199 <lyphuoccong2711@gmail.com>
2021-12-06 23:21:37 +0700

    move configserver

--- src/main/java/com/example/Warehouse/config/GlobalConfig.java
+++ src/main/java/com/example/Warehouse/config/GlobalConfig.java
@@ -1,39 +1,40 @@
-package com.example.Warehouse.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-
-@Component
-@PropertySource("classpath:application.yml")
-@ConfigurationProperties
-public class GlobalConfig {
-	private String host;
-	private String username;
-	private String password;
-
-	public String getHost() {
-		return host;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-}
+//for configserver
+//package com.example.Warehouse.config;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@PropertySource("classpath:application.yml")
+//@ConfigurationProperties
+//public class GlobalConfig {
+//	private String host;
+//	private String username;
+//	private String password;
+//
+//	public String getHost() {
+//		return host;
+//	}
+//
+//	public void setHost(String host) {
+//		this.host = host;
+//	}
+//
+//	public String getUsername() {
+//		return username;
+//	}
+//
+//	public void setUsername(String username) {
+//		this.username = username;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//}
